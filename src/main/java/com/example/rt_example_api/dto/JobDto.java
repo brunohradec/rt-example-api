9d4f1fadfe0af3a3da7f0b3978b4792e456cdcfe
Bruno Hradec <bhradec@gmail.com>
2025-01-04 20:02:12 +0100

    Add CORS config, add missing ID to DTOs

--- src/main/java/com/example/rt_example_api/dto/JobDto.java
+++ src/main/java/com/example/rt_example_api/dto/JobDto.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class JobDto {
+    private Long id;
     private String name;
     private String description;
     private JobType type;
